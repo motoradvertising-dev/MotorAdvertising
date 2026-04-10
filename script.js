0531f78ecf9d48aec470e4abc5f6ad43df32d41c
Amadeus Arteaga <amadeusguitarte@gmail.com>
2026-04-10 00:46:26 -0500

    fix: conversational balloons sprout from character sides

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     entry.target.classList.remove("visible");
                 }
             });
-        }, { threshold: 0.5 });
+        }, { rootMargin: "-35% 0px -35% 0px" });
         messages.forEach(msg => observer.observe(msg));
     }
 });
